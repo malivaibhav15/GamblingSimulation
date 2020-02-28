e95e6a5d2311347e5a8d398543c55c619525cc8a
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-28 19:20:14 +0530

    ADDED : Give lucky and unlucky day

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,6 +2,10 @@
 stake=100
 win=0
 loose=0
+temp=0
+winAmount=0
+looseAmount=0
+declare -a array
 echo "Welcome to gambling game"
 function winOrLoose()
 {
@@ -25,19 +29,38 @@ function winOrLoose()
 }
 function monthlyWinOrLoose()
 {
+	max=0
+	min=1500
 	read -p "enter the day=" day
-	for (( i=1; i<=day; i++ ))
+	for (( counter=1; counter<=$day; counter++ ))
 	do
    	result=$((RANDOM %2))
    	if [[ $result -eq 1 ]]
    	then
 			((win++))
 			winAmount=$(($winAmount+50))
+			temp=$(($temp+50))
+			array[counter-1]=$temp
+			if [[ $max -lt $temp ]]
+			then
+				max=$temp
+				luckyday=$counter
+			fi
    	else
       	((loose++))
 			looseAmount=$(($looseAmount+50))
+			temp=$(($temp-50))
+			array[counter-1]=$temp
+			if [[ $min -gt $temp ]]
+         then
+            min=$temp
+            unluckyday=$counter
+         fi
   		fi
 	done
+		echo "lucky day=$luckyday"
+		echo "unlucky day=$unluckyday"
+		echo ${array[@]}
 	if [[ $winAmount -gt $looseAmount ]]
 	then
 		echo "At the end of $day days gambler wins $(($winAmount-$looseAmount)) stake"
