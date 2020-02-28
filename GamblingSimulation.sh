77a4565785c2c38a82e19ae0f67d330ccd026840
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-28 17:18:39 +0530

    ADDED : Gives winning or loosing of monthly stake

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 stake=100
+win=0
+loose=0
 echo "Welcome to gambling game"
 function winOrLoose()
 {
@@ -21,4 +23,30 @@ function winOrLoose()
 	echo "Stake at the end of the 20 days play:$amount"
 
 }
+function monthlyWinOrLoose()
+{
+	read -p "enter the day=" day
+	for (( i=1; i<=day; i++ ))
+	do
+   	result=$((RANDOM %2))
+   	if [[ $result -eq 1 ]]
+   	then
+			((win++))
+			winAmount=$(($winAmount+50))
+   	else
+      	((loose++))
+			looseAmount=$(($looseAmount+50))
+  		fi
+	done
+	if [[ $winAmount -gt $looseAmount ]]
+	then
+		echo "At the end of $day days gambler wins $(($winAmount-$looseAmount)) stake"
+	else
+		echo "At the end of $day days gambler looses $(($looseAmount-$winAmount)) stake"
+	fi
+	echo "Gambler wins $win days"
+	echo "Gambler loose $loose days"
+
+}
 winOrLoose
+monthlyWinOrLoose
