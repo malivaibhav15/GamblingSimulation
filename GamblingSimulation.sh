fab9e13f493f2e00b1cfcac73645b317b6f26716
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-28 14:44:03 +0530

    ADDED : Display Winning or loosing

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,4 +2,16 @@
 stake=100
 bet=1
 echo "Welcome to gambling game"
-
+function winOrLoose()
+{
+	bet=$((RANDOM%2))
+	if [[ $bet -eq 1 ]]
+	then
+		((stake++))
+		echo "Current stake="$stake
+	else
+		((stake--))
+		echo "Current stake="$stake
+	fi
+}
+winOrLoose
