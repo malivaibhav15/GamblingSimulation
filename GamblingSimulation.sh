bed1dd568459b28106c86cf628cb44624bdb4164
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-28 15:32:36 +0530

    ADDED : Display total amount during the period

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -3,16 +3,22 @@ stake=100
 echo "Welcome to gambling game"
 function winOrLoose()
 {
-	while [[ $stake -le 149 && $stake -ge 51 ]]
+	for (( counter=0;counter<20;counter++ ))
 	do
-		bet=$((RANDOM%2))
-		if [[ $bet -eq 1 ]]
-		then
-			((stake++))
-		else
-			((stake--))
-		fi
+		while [[ $stake -le 149 && $stake -ge 51 ]]
+		do
+			bet=$((RANDOM%2))
+			if [[ $bet -eq 1 ]]
+			then
+				((stake++))
+			else
+				((stake--))
+			fi
+		done
+		amount=$(( $amount+$stake ))
 	done
 	echo "Stake at end of the day's play:$stake"
+	echo "Stake at the end of the 20 days play:$amount"
+
 }
 winOrLoose
